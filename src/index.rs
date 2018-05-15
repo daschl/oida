5b4db2d6e57e3251f6904d05f4ab86ff8aef91c2
Michael Nitschinger <michael@nitschinger.at>
2018-05-15 17:14:35 +0200

    Parse client init config

--- src/index.rs
+++ src/index.rs
@@ -1,9 +1,10 @@
+use chrono::NaiveDateTime;
 use parse::LogLine;
 use regex::Regex;
-use chrono::NaiveDateTime;
 use serde_cbor::{from_reader, to_writer};
-use std::io::{Read, Write};
 use std::collections::HashMap;
+use std::io::{Read, Write};
+use std::iter::FromIterator;
 
 lazy_static! {
     static ref CONNECTING_NODE: Regex = Regex::new(r"Connecting Node ([^/]+)").unwrap();
@@ -92,28 +93,32 @@ impl EventIndex {
     }
 
     fn extract_client_details(&self, input: &str) -> HashMap<String, String> {
-        let data = HashMap::new();
+        lazy_static! {
+            static ref KV_MAP_R: Regex = Regex::new(r"^[a-zA-Z]+=").unwrap();
+        };
 
-        // TODO: figure out how to parse this blob properly...
-        // sslEnabled=false, sslKeystoreFile='null', sslKeystorePassword=false, sslKeystore=null, bootstrapHttpEnabled=true, 
-        // bootstrapCarrierEnabled=true, bootstrapHttpDirectPort=8091, bootstrapHttpSslPort=18091, 
-        // bootstrapCarrierDirectPort=11210, bootstrapCarrierSslPort=11207, ioPoolSize=3, computationPoolSize=3, 
-        // responseBufferSize=16384, requestBufferSize=16384, kvServiceEndpoints=1, viewServiceEndpoints=1, 
-        // queryServiceEndpoints=1, searchServiceEndpoints=1, ioPool=NioEventLoopGroup, coreScheduler=CoreScheduler, 
-        // memcachedHashingStrategy=DefaultMemcachedHashingStrategy, eventBus=DefaultEventBus, 
-        // packageNameAndVersion=couchbase-java-client/2.3.7 (git: 2.3.7, core: 1.3.7-dirty), dcpEnabled=false, 
-        // retryStrategy=BestEffort, maxRequestLifetime=75000, 
-        // retryDelay=ExponentialDelay{growBy 1.0 MICROSECONDS, powers of 2; lower=100, upper=100000}, 
-        // reconnectDelay=ExponentialDelay{growBy 1.0 MILLISECONDS, powers of 2; lower=32, upper=4096}, 
-        // observeIntervalDelay=ExponentialDelay{growBy 1.0 MICROSECONDS, powers of 2; lower=10, upper=100000}, 
-        // keepAliveInterval=30000, autoreleaseAfter=2000, bufferPoolingEnabled=true, tcpNodelayEnabled=true, 
-        // mutationTokensEnabled=false, socketConnectTimeout=1000, dcpConnectionBufferSize=20971520, 
-        // dcpConnectionBufferAckThreshold=0.2, dcpConnectionName=dcp/core-io, callbacksOnIoPool=false, 
-        // disconnectTimeout=25000, requestBufferWaitStrategy=com.couchbase.client.core.env.DefaultCoreEnvironment$2@5426e924, 
-        // queryTimeout=75000, viewTimeout=75000, kvTimeout=2000, connectTimeout=3000, dnsSrvEnabled=true
-        println!("{}", input);
+        let mut lines = vec![];
+        let mut offset = 0;
+        while let Some(i) = input[offset..].find(',') {
+            let line = &input[offset..offset + i];
+            lines.push(line.trim());
+            offset = offset + i + 1;
+        }
 
-        data
+        let mut pairs: Vec<(String, String)> = vec![];
+        for line in lines {
+            if line.ends_with('}') || !KV_MAP_R.is_match(&line) {
+                match pairs.last_mut() {
+                    Some(v) => v.1.push_str(line),
+                    None => (),
+                }
+            } else {
+                let parts: Vec<&str> = line.split('=').collect();
+                pairs.push((parts[0].into(), parts[1].into()));
+            }
+        }
+
+        HashMap::from_iter(pairs.into_iter())
     }
 
     pub fn serialize<W: Write>(&self, writer: &mut W) {
--- src/main.rs
+++ src/main.rs
@@ -12,17 +12,17 @@ extern crate colored;
 extern crate serde_cbor;
 extern crate toml;
 
-mod parse;
-mod index;
 mod config;
+mod index;
+mod parse;
 
 use clap::{App, ArgMatches};
+use config::{Config, ShowFormat};
+use index::{ClientEvent, EventIndex, TopologyEvent};
 use parse::Parser;
 use pbr::{ProgressBar, Units};
-use index::{EventIndex, TopologyEvent};
-use config::{ShowFormat, Config};
-use std::io::prelude::*;
 use std::fs::File;
+use std::io::prelude::*;
 use std::io::{BufRead, BufReader};
 use std::time::Duration;
 
@@ -53,14 +53,14 @@ fn check(matches: &ArgMatches) {
     let pattern = check_config.pattern;
     let parser = Parser::new(&pattern).expect("Could not init parser");
     let mut index = EventIndex::new();
-    
+
     let file = File::open(&check_config.input).expect("could not open file");
     let len = file.metadata().unwrap().len();
     let mut reader = BufReader::new(file);
 
     let mut pb = ProgressBar::new(len);
     pb.set_units(Units::Bytes);
-    // Don't refresh more often so contention is on stdout which 
+    // Don't refresh more often so contention is on stdout which
     // will actually slow the whole thing down...
     pb.set_max_refresh_rate(Some(Duration::from_millis(100)));
     pb.format("[-> ]");
@@ -72,11 +72,13 @@ fn check(matches: &ArgMatches) {
         match num_bytes {
             Ok(num) if num > 0 => {
                 pb.add(num as u64);
-                let parsed = parser.parse(&line).expect(&format!("Could not parse line {:?}", line));
+                let parsed = parser
+                    .parse(&line)
+                    .expect(&format!("Could not parse line {:?}", line));
                 if parsed.is_some() {
                     index.feed(parsed.unwrap());
                 }
-            },
+            }
             Ok(_) => break,
             Err(e) => panic!(format!("Error wile decoding line! {:?}", e)),
         }
@@ -115,7 +117,20 @@ fn show_cli(index: EventIndex) {
 
     println!("> Printing Stats to CLI\n");
 
-    println!("Topology Events");
+    println!("Client Events");
+    println!("---------------\n");
+
+    for ev in index.client_events {
+        match ev {
+            ClientEvent::ClientInit(d, i) => println!(
+                "  {} Client Init Config: {:?}",
+                d.format("%H:%M:%S").to_string(),
+                i
+            ),
+        }
+    }
+
+    println!("\nTopology Events");
     println!("---------------\n");
 
     for ev in index.topo_events {
